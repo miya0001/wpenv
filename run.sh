4f4a28acc370118e42464fa34161b01e2fb77f85
miya0001 <miya@wpist.me>
2017-09-22 22:57:51 +0900

    run if install-wp-tests.sh exists

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ PHP_VERSION=$1
 CONTAINER_NAME=${2-wpdev}
 
 docker pull miya0001/phpenv:$PHP_VERSION
-docker run -idt --name $CONTAINER_NAME --privileged --volume="$(pwd)":/shared/:rw miya0001/phpenv:$PHP_VERSION "/bin/bash"
+docker run -idt --name $CONTAINER_NAME --privileged -w /shared --volume="$(pwd)":/shared/:rw miya0001/phpenv:$PHP_VERSION "/bin/bash"
 
 # Init MySQL
 docker exec $CONTAINER_NAME sudo chown -R mysql:mysql /var/lib/mysql
@@ -21,7 +21,10 @@ docker exec $CONTAINER_NAME sudo service mysql start
 docker exec $CONTAINER_NAME ln -s /var/run/mysqld/mysqld.sock /tmp/mysql.sock || echo "/tmp/mysql.sock already exists."
 
 # Creates MySQL database for WordPress
-docker exec $CONTAINER_NAME mysql -u root -e "CREATE DATABASE wordpress_tests;" -p1111
 docker exec $CONTAINER_NAME mysql -u root -e 'GRANT ALL PRIVILEGES ON wp_cli_test.* TO "wp_cli_test"@"localhost" IDENTIFIED BY "password1";' -p1111
+
+if [ -e bin/install-wp-tests.sh ]; then
+  docker exec $CONTAINER_NAME bash bin/install-wp-tests.sh wordpress_tests root 1111
+fi
 
 echo "Run: docker exec -it $CONTAINER_NAME bash"
